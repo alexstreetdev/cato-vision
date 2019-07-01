2666cf44a3bc6142f8b000b8239f9b38ad809741
alexstreetdev <alexstreetdev@users.noreply.github.com>
2019-07-01 14:14:27 +0100

    Additional Image Content fields

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 class ImageContent:
-    def __init__(self, id, url, x, y, width, height, description):
+    def __init__(self, id, url, x, y, width, height, description, data, source):
         self.ImageId = id
         self.ImageUrl = url
         self.X = x
@@ -7,3 +7,5 @@ class ImageContent:
         self.Width = width
         self.Height = height
         self.ContentDescription = description
+        self.ContentData = data
+        self.Source = source
